4739567c7e51b9e27359094885db9b847c654f9a
purush2300 <reddypurushothm@gmail.com>
2026-04-09 22:43:49 +0530

    Initial comit

---
+++ Basic3.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Basic3 {
+    public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+
+        System.out.println("Matrix-A row length");
+        int row1=sc.nextInt();
+
+        System.out.println("Matrix-A column length");
+        int col1=sc.nextInt();
+
+        int [][]a=new int[row1][col1];
+        System.out.println("Enter Matrix-A one side other");
+
+            for (int i = 0; i <row1; i++) {
+                for (int j = 0; j <col1; j++) {
+                    a[i][j]=sc.nextInt();
+                }
+            }
+
+                int k=0;
+             System.out.println("Matrix-A values");
+            for (int i = 0; i < row1; i++) {
+                for (int j = 0; j < col1; j++) {
+                  
+                  k=k+a[j][i];
+                 
+                  
+                }
+                break;
+                
+            }
+            System.out.println(k);
+
+
+
+    }
+}
