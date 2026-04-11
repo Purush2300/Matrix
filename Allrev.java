d5c9f618ab0fffaf792c3baab447d70f721c7fff
purush2300 <reddypurushothm@gmail.com>
2026-04-11 23:16:49 +0530

    Initial commit

--- Allrev.java
+++ Allrev.java
@@ -1,22 +1,33 @@
 public class Allrev {
     public static void main(String[] args) {
         
-int []a={1, 2, 2, 4, 3, 1, 4};
+int []a={2, 4, 5, -1, -3, -4};
+int []pos= new int[a.length/2];
+int []neg=new int[a.length/2];
+int p=0;
+int n=0;
+for (int i = 0; i < a.length; i++) {
+    if(a[i]>0){
+        pos[p]=a[i];
+        p++;
+    }
+    else{
+        neg[n]=a[i];
+        n++;
+    }
+}
+int l=0;
+int r=0;
+for (int i = 0; i < a.length/2; i++) {
+    a[2*i]=pos[l];
+    l++;
+    a[2*i+1]=neg[r];
+    r++;
+}
 
-
-       for(int i=0;i<a.length;i++){
-        int count=0;
-        for (int j = 0; j < a.length; j++) {
-            if(a[i]==a[j]){
-                count++;
-            }
-            
-        }
-        if(count==1){
- System.out.println(a[i]);
-        }
-       
-       } 
+      for (int i = 0; i < a.length; i++) {
+        System.out.print(a[i]+" ");
+      }
        
      
 
