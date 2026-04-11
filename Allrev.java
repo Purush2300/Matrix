f634c373a6b4273bd893edc08bcdc249afc460b7
purush2300 <reddypurushothm@gmail.com>
2026-04-11 21:55:26 +0530

    Initial commit

--- Allrev.java
+++ Allrev.java
@@ -1,12 +1,27 @@
 public class Allrev {
     public static void main(String[] args) {
-        int []a={-3,-4,-6,-1,-2,-10};
-        int max=Integer.MAX_VALUE;
-        for (int i = 0; i < a.length; i++) {
-            if (a[i]<max) {
-                max=a[i];
-            }
+        int []a={1,2,3,4,5,6,7};
+      int k=3;
+      int n=a.length;
+      k=k%n;
+        int []temp=new int[k];
+
+        for (int i = 0; i < k; i++) {
+           temp[i] =a[n-k+i];
         }
-        System.out.println(max);
+
+        for (int i = n-k-1; i >=0; i--) {
+            a[i+k]=a[i];
+        }
+
+        for (int i = 0; i <k; i++) {
+            a[i]=temp[i];
+        }
+
+        for (int i = 0; i <n; i++) {
+            System.out.print(a[i]+" ");
+        }
+
+
     }
 }
