cda26cdc41e1663fce0e67103a9ab4934b39d4d4
purush2300 <reddypurushothm@gmail.com>
2026-04-11 22:35:25 +0530

    Initial commit

--- Allrev.java
+++ Allrev.java
@@ -1,27 +1,20 @@
 public class Allrev {
     public static void main(String[] args) {
-        int []a={1,2,3,4,5,6,7};
-      int k=3;
-      int n=a.length;
-      k=k%n;
-        int []temp=new int[k];
-
-        for (int i = 0; i < k; i++) {
-           temp[i] =a[n-k+i];
-        }
-
-        for (int i = n-k-1; i >=0; i--) {
-            a[i+k]=a[i];
-        }
-
-        for (int i = 0; i <k; i++) {
-            a[i]=temp[i];
-        }
-
-        for (int i = 0; i <n; i++) {
-            System.out.print(a[i]+" ");
-        }
-
+        
+int []a={1,1,1,1,1,1};
+int count=0;
+int l=0;
+       for(int i=0;i<a.length;i++){
+     if(a[i]!=0){
+        count++;
+     }
+    l= Math.max(l, count);
+    if(a[i]==0){
+        count=0;
+    }
+       } 
+       
+       System.out.println(l);
 
     }
 }
