e3040283263eb06a72a7054590cc68050c721dc5
purush2300 <reddypurushothm@gmail.com>
2026-04-12 22:37:10 +0530

    Iniial commit

--- Sum.java
+++ Sum.java
@@ -1,18 +1,23 @@
+import java.util.*;
+
 public class Sum {
     public static void main(String[] args) {
+        HashMap<Integer,Integer> map=new HashMap<>();
         int []a={1,2,-1,1,1,3,4,1,2,3};
         int k=3;
         int count=0;
-          
+        int prefix=0;
+        map.put(0,1);
+
+
         for (int i = 0; i < a.length; i++) {
-          int sum=0;
-            for (int j = i; j < a.length; j++) {
-                sum+=a[j];
-                if(sum==k){
-                    count++;
-                }
+            prefix+=a[i];
+            if(map.containsKey(prefix-k)){
+                count+=map.get(prefix-k);
             }
+            map.put(prefix,map.getOrDefault(prefix, 0)+1);
         }
-        System.out.println(count);
+          
+       System.out.println(count);
     }
 }
