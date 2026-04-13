d6f080f2c37e62a2b3fbc4a98b309654a97c2237
purush2300 <reddypurushothm@gmail.com>
2026-04-13 21:34:38 +0530

    Initial commit

--- Basic9.java
+++ Basic9.java
@@ -1,27 +1,22 @@
 public class Basic9 {
     public static void main(String[] args) {
         int []a={1,3,6,7,8,12,14,16};
-        int k=16;
+        int k=31;
        int low=0;
        int high=a.length-1;
-       for (int i = 0; i < a.length; i++) {
-        
-        int mid =(low+high)/2;
+       while (low<=high) {
+        int mid=(low+high)/2;
         if(a[mid]==k){
-            System.out.println(a[mid]);
+            System.out.println(a[mid]+"element found");
             return;
         }
-        if(k<a[mid]){
-            high=mid-1;
-
-        }
-        else{
+        else if(k>a[mid]){
             low=mid+1;
         }
-
-
+        else{
+            high=mid+1;
+        }
        }
-
-System.out.println(a[low]);
+       System.out.println("element not found");
     }
 }
