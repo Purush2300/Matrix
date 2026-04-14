e6721f3cf6082c4c5e6b919de0c6b5c1b0f08ba5
purush2300 <reddypurushothm@gmail.com>
2026-04-14 22:43:41 +0530

    Initial commit

---
+++ Sorted.java
@@ -0,0 +1,35 @@
+public class Sorted {
+    public static void main(String[] args) {
+        int []a={7,8,9,1,2,3,4,5,6};
+        int i=0;
+        int k=1;
+        int j=a.length-1;
+        while (i<=j) {
+            int mid=(i+j)/2;
+            if(a[mid]==k){
+                System.out.println(mid);
+                return;
+            }
+
+            if(a[i]<=a[mid]){
+                if(a[i]<=k && k<=a[mid]){
+                    j=mid-1;
+                }
+                else{
+                    i=mid+1;
+                }
+            }
+
+            else{
+                if(a[mid]<=k && k<=a[j]){
+                    i=mid+1;
+                }
+                else{
+                    j=mid-1;
+                }
+            }
+            
+        }
+        System.out.println(-1);
+    }
+}
