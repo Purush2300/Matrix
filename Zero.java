56bb543aad02c18b3d1dbdfd700db9d974646fdd
purush2300 <reddypurushothm@gmail.com>
2026-04-20 12:49:31 +0530

    Initial comit

--- Zero.java
+++ Zero.java
@@ -1,25 +1,52 @@
 public class Zero {
  public static void main(String[] args) {
-    int[][]a={{1,1,1},{1,0,0},{1,1,1}};
+    int[][]a={{1,0,1},{1,1,1},{1,1,1}};
     int r=a.length;
     int c=a[0].length;
-    for (int i = 0; i <r; i++) {
-        for (int j = 0; j < c; j++) {
-            if(a[i][j]==0){
-                row(a,i,c);
-                col(a,j,r);
+    boolean[] row= new boolean[r];
+    boolean[]col = new boolean[c];
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if(a[i][j]==0){
+                    row[i]=true;
+                    col[j]=true;
+                }
             }
-            
         }
-    }
 
 for (int i = 0; i < r; i++) {
     for (int j = 0; j < c; j++) {
-        if(a[i][j]==-1){
+        if(row[i]||col[j]){
             a[i][j]=0;
         }
     }
 }
+
+
+
+
+
+
+
+
+    // for (int i = 0; i <r; i++) {
+    //     for (int j = 0; j < c; j++) {
+    //         if(a[i][j]==0){
+    //             row(a,i,c);
+    //             col(a,j,r);
+    //         }
+            
+    //     }
+    // }
+
+// for (int i = 0; i < r; i++) {
+//     for (int j = 0; j < c; j++) {
+//         if(a[i][j]==-1){
+//             a[i][j]=0;
+//         }
+//     }
+// }
 
 
 
@@ -33,19 +60,19 @@ for (int i = 0; i < r; i++) {
 
  }
 
- private static void col(int[][] a, int j,int r) {
-   for (int i = 0; i <r; i++) {
-    if(a[i][j]!=0){
-        a[i][j]=-1;
-    }
-   }
-}
+//  private static void col(int[][] a, int j,int r) {
+//    for (int i = 0; i <r; i++) {
+//     if(a[i][j]!=0){
+//         a[i][j]=-1;
+//     }
+//    }
+// }
 
- private static void row(int[][] a, int i,int c) {
-   for (int j = 0; j < c; j++) {
-    if(a[i][j]!=0){
-        a[i][j]=-1;
-    }
-   }
- }   
+//  private static void row(int[][] a, int i,int c) {
+//    for (int j = 0; j < c; j++) {
+//     if(a[i][j]!=0){
+//         a[i][j]=-1;
+//     }
+//    }
+//  }   
 }
