76bb0013a86b8dbce4a7cac3f76adacf45604427
purush2300 <reddypurushothm@gmail.com>
2026-07-27 21:09:49 +0530

    Initial commit

--- Basic.java
+++ Basic.java
@@ -5,7 +5,7 @@ public class Basic {
         Scanner sc= new Scanner(System.in);
         int row=3;
         int col=3;
-        int [][]a={{1,2,3},{4,5,6,},{7,8,9}};
+        int [][]a={{1,2,3},{40,50},{7,8,9}};
 
       
         for (int i = 0; i < row; i++) {
