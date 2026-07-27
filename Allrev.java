3ff1ba378c355c14f8927f72018e9f958a1629fc
purush2300 <reddypurushothm@gmail.com>
2026-07-27 21:08:05 +0530

    Iniital commit

--- Allrev.java
+++ Allrev.java
@@ -1,7 +1,7 @@
 public class Allrev {
     public static void main(String[] args) {
         
-int []a={1, 2, 5, 3, 1, 2};
+int []a={1, 2, 5, 3};
 int max=0;
 for (int i = a.length-1; i >=0; i--) {
     if(a[i]>max){
